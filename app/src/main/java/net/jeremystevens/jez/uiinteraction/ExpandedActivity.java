65a045e452fc739a4c42d0c4efcb2e24a20e6f51
Jez <jez0707@gmail.com>
2016-06-06 22:25:58 +0100

    implemented entry animation on text group

--- app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
+++ app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
@@ -2,6 +2,9 @@ package net.jeremystevens.jez.uiinteraction;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class ExpandedActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class ExpandedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expanded_layout);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.bottom_slide_anim);
+        View textContainer = findViewById(R.id.text_container);
+        if (textContainer != null) {
+            textContainer.startAnimation(animation);
+        }
     }
 }
