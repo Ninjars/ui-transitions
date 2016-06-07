f9dd8ce31e023f9f31c318fee875dc4fd6d791eb
Jez <jez0707@gmail.com>
2016-06-08 00:00:20 +0100

    added three circular images to bottom of expanded view

--- app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
+++ app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
@@ -1,10 +1,15 @@
 package net.jeremystevens.jez.uiinteraction;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class ExpandedActivity extends AppCompatActivity {
 
@@ -17,5 +22,20 @@ public class ExpandedActivity extends AppCompatActivity {
         if (textContainer != null) {
             textContainer.startAnimation(animation);
         }
+
+        ImageView middleView = (ImageView) findViewById(R.id.middle);
+        bindImage(middleView, R.drawable.confused_cat);
+        ImageView leftView = (ImageView) findViewById(R.id.left);
+        bindImage(leftView, R.drawable.sad_cat);
+        ImageView rightView = (ImageView) findViewById(R.id.right);
+        bindImage(rightView, R.drawable.happy_cat);
+    }
+
+    private void bindImage(ImageView view, int resource) {
+        // view may be null, but at least in the short term it's clearer to crash sooner rather than later
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), resource);
+        RoundedBitmapDrawable image = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+        image.setCircular(true);
+        view.setImageDrawable(image);
     }
 }
