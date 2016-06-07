343d3be973a214cf3ec48bd220e27299488d38ae
Jez <jez0707@gmail.com>
2016-06-08 00:09:27 +0100

    added entry animations to circle images

--- app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
+++ app/src/main/java/net/jeremystevens/jez/uiinteraction/ExpandedActivity.java
@@ -17,18 +17,25 @@ public class ExpandedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expanded_layout);
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.bottom_slide_anim);
+        Animation bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_slide_anim);
         View textContainer = findViewById(R.id.text_container);
         if (textContainer != null) {
-            textContainer.startAnimation(animation);
+            textContainer.startAnimation(bottomAnim);
         }
 
         ImageView middleView = (ImageView) findViewById(R.id.middle);
         bindImage(middleView, R.drawable.confused_cat);
+        middleView.startAnimation(bottomAnim);
+
         ImageView leftView = (ImageView) findViewById(R.id.left);
         bindImage(leftView, R.drawable.sad_cat);
+        Animation leftAnim = AnimationUtils.loadAnimation(this, R.anim.left_slide_anim);
+        leftView.startAnimation(leftAnim);
+
         ImageView rightView = (ImageView) findViewById(R.id.right);
         bindImage(rightView, R.drawable.happy_cat);
+        Animation rightAnim = AnimationUtils.loadAnimation(this, R.anim.right_slide_anim);
+        rightView.startAnimation(rightAnim);
     }
 
     private void bindImage(ImageView view, int resource) {
